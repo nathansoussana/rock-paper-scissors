42c10f844c03c9af7170bfe28c0a91b66d49ac19
Nathan Soussana <nathan.soussana@gmail.com>
2023-08-07 14:55:04 +0200

    Added a reset button functionality once one of the players get to 5. Slightly cleaned up the display

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ let computerScore = 0;
 // HTML Elements
 let handsEl = document.getElementById("hands-el");
 let resultEl = document.getElementById("result-el");
-let playButton = document.getElementById("play-btn");
+let playBtn = document.getElementById("play-btn");
+let resetBtn = document.getElementById("reset-btn");;
 let myScoreEl = document.getElementById("my-score-el");
 let computerScoreEl = document.getElementById("computer-score-el");
 
@@ -19,6 +20,11 @@ function playRound() {
   handsEl.textContent = showHands();
   resultEl.textContent = displayResult();
   addPoints();
+
+  if (myScore >= 5 || computerScore >= 5) {
+    playBtn.style.display = "none";
+    resetBtn.style.display = "block";
+  }
 }
 
 function pickHand() {
@@ -62,10 +68,22 @@ function displayResult() {
 
 function addPoints() {
   if (displayResult() === "You win!") {
-    myScore++
-    myScoreEl.textContent = `You: ${myScore}`
+    myScore++;
+    myScoreEl.textContent = `You: ${myScore}`;
+    
   } else if (displayResult() === "Computer wins!") {
-    computerScore++
-    computerScoreEl.textContent = `Computer: ${computerScore}`
+    computerScore++;
+    computerScoreEl.textContent = `Computer: ${computerScore}`;
   }
+}
+
+function resetGame() {
+  myScore = 0;
+  computerScore = 0;
+
+  myScoreEl.textContent = `You: ${myScore}`;
+  computerScoreEl.textContent = `Computer: ${computerScore}`;
+
+  resetBtn.style.display = "none";
+  playBtn.style.display = "block";
 }
