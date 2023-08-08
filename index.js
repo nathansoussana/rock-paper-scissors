e9ae4c00086f4107c7a21b6eee081f9e6f1a0d36
Nathan Soussana <nathan.soussana@gmail.com>
2023-08-08 12:01:25 +0200

    Modified the gameplay: player now has to choose for his hand himself. Modified layout with score on top, hands in the middle, and choice of hands at the bottom. Started with CSS

--- index.js
+++ index.js
@@ -5,63 +5,91 @@ let playerTwoHand;
 let myScore = 0;
 let computerScore = 0;
 
+
 // HTML Elements
+let myHand = document.getElementById("my-hand");
+let computerHand = document.getElementById("computer-hand");
+
 let handsEl = document.getElementById("hands-el");
 let resultEl = document.getElementById("result-el");
+
+let rockBtn = document.getElementById("rock-btn");
+let paperBtn = document.getElementById("paper-btn");
+let scissorsBtn = document.getElementById("scissors-btn");
+
 let playBtn = document.getElementById("play-btn");
 let restartBtn = document.getElementById("restart-btn");;
+
 let myScoreEl = document.getElementById("my-score-el");
 let computerScoreEl = document.getElementById("computer-score-el");
 
+
 // Functions & Rules
+function pickRock() {
+  playerOneHand = "🪨"
+  playRound()
+}
+function pickPaper() {
+  playerOneHand = "📄"
+  playRound()
+}
+function pickScissors() {
+  playerOneHand = "✂️"
+  playRound()
+}
+
 function playRound() {
-  playerOneHand = pickHand();
-  playerTwoHand = pickHand();
-  handsEl.textContent = displayHands();
-  resultEl.textContent = computeResult();
+  computerChoice();
+  displayHands();
+  computeResult();
   addPoints();
 
   if (myScore >= 5 || computerScore >= 5) {
-    playBtn.style.display = "none";
+    rockBtn.style.display = "none";
+    paperBtn.style.display = "none";
+    scissorsBtn.style.display = "none";
     restartBtn.style.display = "block";
   }
 }
 
-function pickHand() {
+function computerChoice() {
   let i = Math.floor(Math.random() * hands.length);
-  return hands[i];
+  playerTwoHand = hands[i];
+  return playerTwoHand
 }
 
 function displayHands() {
+  myHand.textContent = playerOneHand;
+  computerHand.textContent = computerChoice();
   return `${playerOneHand} vs ${playerTwoHand}`;
 }
 
 function computeResult() {
   if (playerOneHand === "🪨") {
     if (playerTwoHand === "🪨") {
-      return "Tie!";
+      return resultEl.textContent = "Tie!";
     } else if (playerTwoHand === "📄") {
-      return "Computer wins!";
+      return resultEl.textContent = "Computer wins!";
     } else if (playerTwoHand === "✂️") {
-      return "You win!";
+      return resultEl.textContent = "You win!";
     }
 
   } else if (playerOneHand === "📄") {
     if (playerTwoHand === "🪨") {
-      return "You win!";
+      return resultEl.textContent = "You win!";
     } else if (playerTwoHand === "📄") {
-      return "Tie!";
+      return resultEl.textContent = "Tie!";
     } else if (playerTwoHand === "✂️") {
-      return "Computer wins!";
+      return resultEl.textContent = "Computer wins!";
     }
 
   } else if (playerOneHand === "✂️") {
     if (playerTwoHand === "🪨") {
-      return "Computer wins!";
+      return resultEl.textContent = "Computer wins!";
     } else if (playerTwoHand === "📄") {
-      return "You win!";
+      return resultEl.textContent = "You win!";
     } else if (playerTwoHand === "✂️") {
-      return "Tie!";
+      return resultEl.textContent = "Tie!";
     }
   }
 }
@@ -69,11 +97,11 @@ function computeResult() {
 function addPoints() {
   if (computeResult() === "You win!") {
     myScore++;
-    myScoreEl.textContent = `You: ${myScore}`;
+    myScoreEl.textContent = myScore;
 
   } else if (computeResult() === "Computer wins!") {
     computerScore++;
-    computerScoreEl.textContent = `Computer: ${computerScore}`;
+    computerScoreEl.textContent = computerScore;
   }
 }
 
@@ -81,9 +109,11 @@ function restartGame() {
   myScore = 0;
   computerScore = 0;
 
-  myScoreEl.textContent = `You: ${myScore}`;
-  computerScoreEl.textContent = `Computer: ${computerScore}`;
+  myScoreEl.textContent = myScore;
+  computerScoreEl.textContent = computerScore;
 
   restartBtn.style.display = "none";
-  playBtn.style.display = "block";
+  rockBtn.style.display = "inline";
+  paperBtn.style.display = "inline";
+  scissorsBtn.style.display = "inline";
 }
